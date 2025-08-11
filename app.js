14835220a74d2291fa5d65e10b7d009beb681024
shruti64383 <theshrutisharmaa@gmail.com>
2025-08-11 21:18:07 +0530

    preflight ....

--- app.js
+++ app.js
@@ -25,10 +25,12 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Content-Disposition']
 }));
+
+app.options("*", cors());  
 
 app.use(cookieParser());
 app.use(express.json({limit: '10mb'}));
