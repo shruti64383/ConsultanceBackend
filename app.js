13de5e30e29882cde08e64d8088ec15011a4fd46
shruti64383 <theshrutisharmaa@gmail.com>
2025-08-11 18:16:50 +0530

    using GridFs ..

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
